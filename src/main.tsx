b3d6b7567f147a259b1ada8c483caf870afc3261
Grover228 <groveraleksej@gmail.com>
2026-08-28 17:16:10 +0300

    Update PWA branding and splash screen

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 );
+
+requestAnimationFrame(() => {
+  requestAnimationFrame(() => {
+    const splash = document.getElementById("app-splash");
+    splash?.classList.add("app-splash--hide");
+    window.setTimeout(() => splash?.remove(), 260);
+  });
+});
